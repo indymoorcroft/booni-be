180f6dde2a601433d32a6ef7ddaa3c18c9184112
indymoorcroft <indigo.moorcroft@hotmail.co.uk>
2025-06-19 11:26:07 +0100

    springboot set up

---
+++ src/main/java/com/example/booni_box/BooniBoxApplication.java
@@ -0,0 +1,13 @@
+package com.example.booni_box;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooniBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooniBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/booni_box/BooniBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booni_box;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooniBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
